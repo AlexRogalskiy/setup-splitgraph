6044a232111b4dcaf98bdffd3b7214bab8ec6e3a
Artjoms Iskovs <mildbyte@gmail.com>
2021-12-08 16:03:33 +0000

    Make wget quiet, quick cleanup

--- setup_splitgraph.sh
+++ setup_splitgraph.sh
@@ -11,7 +11,7 @@ SPLITGRAPH_API_SECRET=$1
 SGR_URL=https://raw.githubusercontent.com/splitgraph/setup-splitgraph/master/sgr
 
 echo "Downloading sgr from $SGR_URL..."
-wget https://raw.githubusercontent.com/splitgraph/setup-splitgraph/master/sgr
+wget --quiet https://raw.githubusercontent.com/splitgraph/setup-splitgraph/master/sgr
 chmod +x sgr
 
 echo "Adding sgr to \$PATH..."
@@ -21,5 +21,5 @@ echo "Setting up $SPLITGRAPH_DEPLOYMENT_URL"
 sgr cloud add --remote splitgraph "$SPLITGRAPH_DEPLOYMENT_URL"
 
 if [[ -n $SPLITGRAPH_API_KEY && -n $SPLITGRAPH_API_SECRET ]]; then
-  ./sgr cloud login-api --remote splitgraph --api-key "$SPLITGRAPH_API_KEY" --api-secret "$SPLITGRAPH_API_SECRET"
+  sgr cloud login-api --remote splitgraph --api-key "$SPLITGRAPH_API_KEY" --api-secret "$SPLITGRAPH_API_SECRET"
 fi
